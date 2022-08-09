03c9f96a36c86e630cfbc35590999f3bd1d84dd4
choiyoungae <choiaeyoung1001@gmail.com>
2022-08-09 17:22:30 +0900

    게시물 리스트 시 데이터를 DB로부터 가져오기

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -3,15 +3,16 @@ package com.KoreaIT.example.JAM;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		ArrayList<Article> articles = new ArrayList<>();
 		int lastArticleId = 0;
 		
 		while(true) {
@@ -33,10 +34,23 @@ public class Main {
 				
 				linkDBMS(title, body);
 				
+			} else if(cmd.equals("article list")) {
+				List<Article> theArticles = selectDBMS();
+				
+				if(theArticles.size() == 0) {
+					System.out.println("게시글이 없습니다.");
+					continue;
+				}
+				
+				System.out.println("  번호  |  제목  ");
+				for(Article article : theArticles) {
+					System.out.printf("  %d  |  %s\n", article.id, article.title);
+				}
 			}
 		}
 		
 	}
+	
 	public static void linkDBMS(String title, String body) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -54,7 +68,7 @@ public class Main {
 			sql += ", title = CONCAT('" + title + "')";
 			sql += ", `body` = CONCAT('" + body + "')";
 			
-			System.out.println(sql);
+//			System.out.println(sql);
 			pstmt = conn.prepareStatement(sql);
 			
 			int affectedRows = pstmt.executeUpdate();
@@ -81,5 +95,70 @@ public class Main {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	public static List<Article> selectDBMS() {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		List<Article> articles = new ArrayList<>();
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://127.0.0.1:3306/article_manager?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+			conn = DriverManager.getConnection(url, "root", "");
+			System.out.println("연결 성공!");
+			
+			String sql = "SELECT * ";
+			sql += "FROM article ";
+			sql += "ORDER BY id DESC";
+			
+//			System.out.println(sql);
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String regDate = rs.getString("regDate");
+				String updateDate = rs.getString("updateDate");
+				String title = rs.getString("title");
+				String body = rs.getString("body");
+				
+				Article article = new Article(id, regDate, updateDate, title, body);
+				articles.add(article);
+			}
+			
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패");
+		} catch (SQLException e) {
+			System.out.println("에러: " + e);
+		} finally {
+			try {
+				if (rs != null && !rs.isClosed()) {
+					rs.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				if (pstmt != null && !pstmt.isClosed()) {
+					pstmt.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return articles;
 	}
 }
